39dbda6bf0e2ac65eb002f58c9078201fba99a3d
Todd Guerra <tguerra@adobe.com>
2019-01-15 12:36:36 -0500

    help menu has a menu now

--- cmds/help.js
+++ cmds/help.js
@@ -1,3 +1,28 @@
+const menus = {
+    main: `
+    aem-spa [command] <options>
+
+    create .............. create a new AEM SPA project
+    generate .............. generate a new item for the project 
+    version ............ show package version
+    help ............... show help menu for a command`,
+
+    create: `
+    aem-spa create <options>
+
+    --framework, -f ..... the framework to use, accepted options are angular or react`,
+
+    generate: `
+    aem-spa generate <options>
+
+    --item, -i ..... the item to generate, accepted options are component or template`,
+
+};
+
 module.exports = (args) => {
-    console.log('help will go here');
+    const subCmd = args._[0] === 'help'
+        ? args._[1]
+        : args._[0];
+
+    console.log(menus[subCmd] || menus.main);
 };
